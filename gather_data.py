443a9c2fd24818fd065796a2c7bd16bd0c2b7721
GuyCalloway <guyfcalloway@gmail.com>
2020-03-24 12:15:01 +0000

    Structuring backend project files to gather data, some scrap working prior to testing

--- gather_data.py
+++ gather_data.py
@@ -1,7 +1,11 @@
-from APIcall import api_call
+from darksky_API_call import forecast
+from googlemaps_API_call import find_lat_and_lon
+from keys import *
 
-secret_key = 'Secret KEY HERE'
-# refactor to Environmental Variable
-longitude = -79.3892455
-latitude = 43.6425701
 exclude = 'minutely, hourly, daily, alerts, flags'
+lat_long_tuple = (-79.3892455, 43.6425701)
+
+# data1 = find_lat_and_lon(location, googlemaps_key)
+data2 = forecast(darksky_key, lat_lon_tuple, exclude)
+
+print(data2)
--- googlemaps_API_call.py
+++ googlemaps_API_call.py
@@ -1,9 +1,8 @@
 import requests
-from keys import map_api_key
 
 
-def find_lat_and_lon(location, map_api_key):
+def find_lat_and_lon(location, googlemaps_key):
     url = f'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY'
     response = requests.get(url)
     dictionary = json.dumps(response.json(), sort_keys=True, indent=4)
-    print(dictionary)
+    return dictionary
