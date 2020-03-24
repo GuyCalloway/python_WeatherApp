5eff02265bf58ccac29cadfa3241936c94ae2617
GuyCalloway <guyfcalloway@gmail.com>
2020-03-24 14:56:44 +0000

    Refactor for consistency

--- darksky_API_call.py
+++ darksky_API_call.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-def forecast(darksky_key, lat_lon, exclude):
+def forecast(lat_lon, exclude, darksky_key):
     url = f'https://api.darksky.net/forecast/{darksky_key}/{lat_lon[0]},{lat_lon[1]}?exclude={exclude}'
     response = requests.get(url)
     dictionary = json.dumps(response.json(), sort_keys=True, indent=4)
--- gather_data.py
+++ gather_data.py
@@ -2,12 +2,12 @@ from darksky_API_call import forecast
 from googlemaps_API_call import find_lat_and_lon
 from keys import *
 
-# exclude = 'minutely, hourly, daily, alerts, flags'
+exclude = 'minutely, hourly, daily, alerts, flags'
 
 location = "CB215NX"
 
 lat_lon = find_lat_and_lon(location, googlemaps_key)
-forecast_data = forecast(darksky_key, lat_lon, exclude)
+forecast_data = forecast(lat_lon, exclude, darksky_key)
 
 print(lat_lon)
 print(forecast_data)
